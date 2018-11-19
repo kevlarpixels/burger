f5e1dae2af544ae3bda054165aaca2d47c15d5c3
kevlarpixels <kevingaguilar1@gmail.com>
2018-11-18 18:45:04 -0800

    update

---
+++ models/burger.js
@@ -0,0 +1,30 @@
+//Inside your burger directory, create a folder named models.
+
+
+//In models, make a burger.js file.
+//Inside burger.js, import orm.js into burger.js
+
+//Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
+//Export at the end of the burger.js file.
+
+var orm = require('../config/orm');
+
+var burger = {
+	all: function (cb) {
+		orm.selectAll('burgers', function (res) {
+			cb(res);
+		});
+	},
+	create: function (col, burger_name, cb) {
+		orm.insertOne('burgers', burger_name, function (res) {
+			cb(res);
+		});
+	},
+	update: function (objColVals, condition, cb) {
+		orm.updateOne('burgers', objColVals, condition, function (res) {
+			cb(res);
+		});
+	}
+};
+
+module.exports = burger;
